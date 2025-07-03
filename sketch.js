25286377db131f5463bb16673d6ef43c5b3cce25
TatyanneLazier <tatyanne.lazier@escola.pr.gov.br>
2025-07-03 20:05:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400,WEBGL);
+}
+
+function draw() {
+  background(220);
+  circle(8,8,300);
+  sphere(50);
+  text('ente',190,200);
+line(50,50,200,150);
+
+}
